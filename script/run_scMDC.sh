2f8270b834d46acc08eda3958a69f7b3bd7b440b
Xiang Lin <79429595+xianglin226@users.noreply.github.com>
2021-04-22 19:33:53 -0400

    Update run_scMDC.sh

--- script/run_scMDC.sh
+++ script/run_scMDC.sh
@@ -5,7 +5,7 @@ f=../datasets/10XPBMC_filtered_1000G.H5
 echo "10XPBMC"
 for i in {1..10}
  do
-   python -u ../src/run_scMDC.py --n_clusters 7 --ae_weight_file ./AE_weights/AE_weights_GSE128639.pth.tar --data_file $f \
+   python -u ../src/run_scMDC.py --n_clusters 7 --ae_weight_file ./AE_weights/AE_weights_10XPBMC.pth.tar --data_file $f \
    #--embedding_file ./real_embedding/MAE/GSE128639/$i --prediction_file ./real_embedding/MAE/GSE128639/$i
 done
 
@@ -13,6 +13,6 @@ f=../datasets/GSE100866.filtered_1000G.H5
 echo "GSE100866"
 for i in {1..10}
  do
-   python -u ../src/run_scMDC.py --n_clusters 6 --ae_weight_file ./AE_weights/AE_weights_GSE128639.pth.tar --data_file $f \
+   python -u ../src/run_scMDC.py --n_clusters 6 --ae_weight_file ./AE_weights/AE_weights_GSE100866.pth.tar --data_file $f \
    #--embedding_file ./real_embedding/MAE/GSE128639/$i --prediction_file ./real_embedding/MAE/GSE128639/$i
 done
