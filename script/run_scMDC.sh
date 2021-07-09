c45bf4bdba9a4a1f2dd95b7ed41ad0a6261890f6
Xiang Lin <79429595+xianglin226@users.noreply.github.com>
2021-07-08 23:23:30 -0400

    Update run_scMDC.sh

--- script/run_scMDC.sh
+++ script/run_scMDC.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -l
 #SBATCH --gres=gpu:1
 
+#Here is the commands for the real data experiments. We test ten times on each dataset.
+
 f=../datasets/10XPBMC_filtered_1000G.H5
 echo "10XPBMC"
 for i in {1..10}
  do
    python -u ../src/run_scMDC.py --n_clusters 7 --ae_weight_file ./AE_weights/AE_weights_10XPBMC.pth.tar --data_file $f \
-   #--embedding_file ./real_embedding/MAE/GSE128639/$i --prediction_file ./real_embedding/MAE/GSE128639/$i
+   #--embedding_file ./real_embedding/MAE/10XPBMC/$i --prediction_file ./real_embedding/MAE/10XPBMC/$i
 done
 
 f=../datasets/GSE100866.filtered_1000G.H5
@@ -14,5 +16,13 @@ echo "GSE100866"
 for i in {1..10}
  do
    python -u ../src/run_scMDC.py --n_clusters 6 --ae_weight_file ./AE_weights/AE_weights_GSE100866.pth.tar --data_file $f \
+   #--embedding_file ./real_embedding/MAE/GSE100866/$i --prediction_file ./real_embedding/MAE/GSE100866/$i
+done
+
+f=../datasets/GSE128639.filtered_1000G.H5
+echo "GSE128639"
+for i in {1..10}
+ do
+   python -u ../src/run_scMDC.py --n_clusters 6 --ae_weight_file ./AE_weights/AE_weights_GSE128639.pth.tar --data_file $f \
    #--embedding_file ./real_embedding/MAE/GSE128639/$i --prediction_file ./real_embedding/MAE/GSE128639/$i
 done
