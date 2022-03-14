039dc56acc0683104877d13385abef172f69aaf1
Xiang Lin <79429595+xianglin226@users.noreply.github.com>
2022-03-14 00:38:13 -0400

    Update run_LRP_script.py

--- script/run_LRP_script.py
+++ script/run_LRP_script.py
@@ -1,3 +1,3 @@
 f=../datasets/GSE128639_BMNC_annodata.h5
-python -u run_scMDC.py --n_clusters 27 --ae_weight_file ./out_bmnc_full/AE_weights_bmnc.pth.tar --data_file $f --prediction_file bmnc --save_dir out_bmnc_full --filter True 
-python -u run_LRP.py --n_clusters 27 --ae_weights ./out_bmnc_full/AE_weights_bmnc.pth.tar --cluster_index_file ./out_bmnc_full/1_pred.csv --data_file $f --save_dir out_bmnc_full --filter 
+python -u run_scMDC.py --n_clusters 27 --ae_weight_file ./out_bmnc_full/AE_weights_bmnc.pth.tar --data_file $f --prediction_file bmnc --save_dir out_bmnc_full --filter1
+python -u run_LRP.py --n_clusters 27 --ae_weights ./out_bmnc_full/AE_weights_bmnc.pth.tar --cluster_index_file ./out_bmnc_full/1_pred.csv --data_file $f --save_dir out_bmnc_full --filter1 
