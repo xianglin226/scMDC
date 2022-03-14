40dd28578b1748b4a581cf6bd7e37d34d1257b9e
Xiang Lin <79429595+xianglin226@users.noreply.github.com>
2022-03-14 00:12:42 -0400

    Update run_scMDC.sh

--- script/run_scMDC.sh
+++ script/run_scMDC.sh
@@ -3,26 +3,17 @@
 
 #Here are the commands for the real data experiments. We test ten times on each dataset.
 
-f=../datasets/10XPBMC_filtered_1000G.H5
-echo "10XPBMC"
-for i in {1..10}
- do
-   python -u ../src/run_scMDC.py --n_clusters 7 --ae_weight_file ./AE_weights/AE_weights_10XPBMC.pth.tar --data_file $f \
-   #--embedding_file ./real_embedding/MAE/10XPBMC/$i --prediction_file ./real_embedding/MAE/10XPBMC/$i
-done
+f=../datasets/GSE128639_BMNC_annodata.h5
+echo "Run CITE-seq BMNC"
+python -u run_scMDC.py --n_clusters 27 --ae_weight_file AE_weights_bmnc.pth.tar --data_file $f --save_dir citeseq_bmnc /
+--embedding_file --prediction_file --filter
 
-f=../datasets/GSE100866.filtered_1000G.H5
-echo "GSE100866"
-for i in {1..10}
- do
-   python -u ../src/run_scMDC.py --n_clusters 6 --ae_weight_file ./AE_weights/AE_weights_GSE100866.pth.tar --data_file $f \
-   #--embedding_file ./real_embedding/MAE/GSE100866/$i --prediction_file ./real_embedding/MAE/GSE100866/$i
-done
+f=../datasets/10X_pbmc_granulocyte_plus.h5
+echo "Run SMAGE-seq PBMC10K"
+python -u run_scMDC.py --n_clusters 12 --ae_weight_file AE_weights_pbmc10k.pth.tar --data_file $f --save_dir atac_pbmc10k /
+--embedding_file --prediction_file --filter --f1 2000 --f2 2000 -el 256 128 64 -dl1 64 128 256 -dl2 64 128 256 -fi1 0.005 -fi2 0.005 -signma2 2.5 -tau .1
 
-f=../datasets/GSE128639.filtered_1000G.H5
-echo "GSE128639"
-for i in {1..10}
- do
-   python -u ../src/run_scMDC.py --n_clusters 6 --ae_weight_file ./AE_weights/AE_weights_GSE128639.pth.tar --data_file $f \
-   #--embedding_file ./real_embedding/MAE/GSE128639/$i --prediction_file ./real_embedding/MAE/GSE128639/$i
-done
+f=../datasets/GSE128639_BMNC_annodata.h5
+echo "Run multi-batch CITE-seq SLN111"
+python -u run_scMDC_batch.py --n_clusters 35 --ae_weight_file AE_weights_sln111.pth.tar --data_file $f --save_dir citeseq_sln111 /
+--embedding_file --prediction_file --filter
