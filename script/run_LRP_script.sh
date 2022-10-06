8897152cf767fc361586fe30176e6e2324fca392
Xiang Lin <79429595+xianglin226@users.noreply.github.com>
2022-10-06 12:28:15 -0400

    Update run_LRP_script.sh

--- script/run_LRP_script.sh
+++ script/run_LRP_script.sh
@@ -1,4 +1,4 @@
 # Run DE analysis (LRP) based on the well-trained scMDC model and its results.
-f=../datasets/GSE128639_BMNC_annodata.h5
+f=../datasets/CITESeq_GSE128639_BMNC_annodata.h5
 python -u run_scMDC.py --n_clusters 27 --ae_weight_file ./out_bmnc_full/AE_weights_bmnc.pth.tar --data_file $f --prediction_file bmnc --save_dir out_bmnc_full --filter1
 python -u run_LRP.py --n_clusters 27 --ae_weights ./out_bmnc_full/AE_weights_bmnc.pth.tar --cluster_index_file ./out_bmnc_full/1_pred.csv --data_file $f --save_dir out_bmnc_full --filter1 
