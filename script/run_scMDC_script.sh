ee73c1fceba8bd01bdace4c23340a68e09994ccc
Xiang Lin <79429595+xianglin226@users.noreply.github.com>
2024-02-08 18:22:39 -0500

    Update run_scMDC_script.sh

--- script/run_scMDC_script.sh
+++ script/run_scMDC_script.sh
@@ -11,7 +11,7 @@ python -u run_scMDC.py --n_clusters 27 --ae_weight_file AE_weights_bmnc.pth.tar 
 f=../datasets/SMAGESeq_10X_pbmc_granulocyte_plus.h5
 echo "Run SMAGE-seq PBMC10K"
 python -u run_scMDC.py --n_clusters 12 --ae_weight_file AE_weights_pbmc10k.pth.tar --data_file $f --save_dir atac_pbmc10k /
---embedding_file --prediction_file --filter1 --filter2 --f1 2000 --f2 2000 -el 256 128 64 -dl1 64 128 256 -dl2 64 128 256 -fi1 0.005 -fi2 0.005 -signma2 2.5 -tau .1
+--embedding_file --prediction_file --filter1 --filter2 --f1 2000 --f2 2000 -el 256 128 64 -dl1 64 128 256 -dl2 64 128 256 -phi1 0.005 -phi2 0.005 -signma2 2.5 -tau .1
 
 f=../datasets/CITESeq_realdata_spleen_lymph_111_anno_multiBatch.h5
 echo "Run multi-batch CITE-seq SLN111"
